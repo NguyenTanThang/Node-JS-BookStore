5c6b068389be88e19a0cbbf165198e4131f46ae9
ThangNguyenTan <nguyentanthang9a4@gmail.com>
2022-11-22 10:30:30 +0700

    fix: remove use create index for mongoose

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/book-store-api", {
-            useCreateIndex: true,
+        const conn = await mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://thefatterman:123456a@react-online-shopping.mlnqa.mongodb.net/book-store-v1?retryWrites=true&w=majority", {
+            // useCreateIndex: true,
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
